a179314729102ca5916931b85824a2cec19812e3
guilhermetabordaribas <guilherme.ribas@gmail.com>
2017-05-09 21:39:24 -0300

    Update dim_cabos_interface.py

--- dim_cabos_interface.py
+++ dim_cabos_interface.py
@@ -1,10 +1,28 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'dim_cabos.ui'
-#
-# Created by: PyQt5 UI code generator 5.8.1
-#
-# WARNING! All changes made in this file will be lost!
+"""
+Copyright (c) 2017 Guilherme Taborda Ribas.
+
+Copyright (c) 2016 Riverbank Computing Limited.
+
+Copyright (c) 2017 The Qt Company.
+
+
+This file is part of DimCabos.
+
+    DimCabos is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or any later version.
+
+    DimCabos is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with EletroCrom.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
 import os
 import sys
 
