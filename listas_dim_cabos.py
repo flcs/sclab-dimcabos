788ba8da46144aa1dee245e0569f4031e0cfdb63
guilhermetabordaribas <guilherme.ribas@gmail.com>
2017-05-09 21:42:10 -0300

    Update listas_dim_cabos.py

--- listas_dim_cabos.py
+++ listas_dim_cabos.py
@@ -1,3 +1,27 @@
+"""
+Copyright (c) 2017 Guilherme Taborda Ribas.
+
+Copyright (c) 2016 Riverbank Computing Limited.
+
+Copyright (c) 2017 The Qt Company.
+
+
+This file is part of DimCabos.
+
+    DimCabos is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or any later version.
+
+    DimCabos is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with EletroCrom.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
 lista_tipo_circuito = ['','Iluminação', 'Força', 'Sinalização', 'Controle']
 
 lista_sistema = ['','Monofásico (F + N)', 'Monofásico com Terra (F + N + T)',
